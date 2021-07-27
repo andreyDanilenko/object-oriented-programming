2b3e49b0835a16b605f67ec6727a0ae68c980014
Andrey <danilenko.a.g@mail.ru>
2021-07-27 18:16:17 +0300

    Начинаем программировать 1.11

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './main.js',
+  entry: './src/main.js',
   output: {
     filename: './bundle.js',
     path: path.resolve(__dirname, 'public')
