66e7204806cc940b4b8bf3b7b793ad83fc7762a0
Andrey <danilenko.a.g@mail.ru>
2021-07-27 18:00:19 +0300

    Начинаем программировать

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
   entry: './main.js',
   output: {
     filename: './bundle.js',
