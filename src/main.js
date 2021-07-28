22d2dec5023198a4cf768577500caea7817664d0
Andrey <danilenko.a.g@mail.ru>
2021-07-28 12:40:28 +0300

    Отрисовываем блок сортировки

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createSiteNavTemplate } from './view/site-nav.js';
+import { createSiteSortTemplate } from './view/site-sort';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
+const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
+const siteFooterElement = document.querySelector('.footer');
 
 render(siteMainElement, createSiteNavTemplate(), 'afterbegin');
+render(siteMainElement, createSiteSortTemplate(), 'beforeend');
