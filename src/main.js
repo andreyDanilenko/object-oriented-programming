ca67447c97fd8d7474ca3041f5c0391eb2718e6d
Andrey <danilenko.a.g@mail.ru>
2021-07-28 12:52:19 +0300

    Отрисовываем профиль

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
+import { createSiteProfileTemplate } from './view/site-header-profile.js';
 import { createSiteNavTemplate } from './view/site-nav.js';
 import { createSiteSortTemplate } from './view/site-sort';
+
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const siteHeaderElement = document.querySelector('.header');
+
+render(siteHeaderElement, createSiteProfileTemplate(), 'beforeend');
+
 const siteMainElement = document.querySelector('.main');
-const siteFooterElement = document.querySelector('.footer');
 
 render(siteMainElement, createSiteNavTemplate(), 'afterbegin');
 render(siteMainElement, createSiteSortTemplate(), 'beforeend');
+
+
+const siteFooterElement = document.querySelector('.footer');
