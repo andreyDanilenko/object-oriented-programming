30850503228f1cd25a2a478685cbfa6453badfd6
Andrey <danilenko.a.g@mail.ru>
2021-07-28 12:10:28 +0300

    Отрисовываем навигацию

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createSiteNavTemplate } from './view/site-nav.js';
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteMainElement = document.querySelector('.main');
+
+render(siteMainElement, createSiteNavTemplate(), 'afterbegin');
+
+
