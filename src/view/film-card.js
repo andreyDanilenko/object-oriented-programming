7e83d26f79c48a20a5c7a85b22c75230c6530032
Andrey <danilenko.a.g@mail.ru>
2021-08-01 22:31:05 +0300

    Заполняем шаблон данными из моков

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -1,19 +1,25 @@
-export const createFilmCardTemplate = () => (
-  `<article class="film-card">
-    <h3 class="film-card__title">The Dance of Life</h3>
-    <p class="film-card__rating">8.3</p>
+export const createFilmCardTemplate = (param) => {
+  String.prototype.trimEllip = function (length) {
+    return this.length > length ? `${this.substring(0, length)}...` : this;
+  };
+
+  const stringArr = (arr) => arr[0];
+
+  return `<article class="film-card">
+    <h3 class="film-card__title">${param.film_info.title}</h3>
+    <p class="film-card__rating">${param.film_info.total_rating}</p>
     <p class="film-card__info">
-      <span class="film-card__year">1929</span>
-      <span class="film-card__duration">1h 55m</span>
-      <span class="film-card__genre">Musical</span>
+      <span class="film-card__year">${param.film_info.release.date}</span>
+      <span class="film-card__duration">${param.film_info.runtime}</span>
+      <span class="film-card__genre">${stringArr(param.film_info.genre)}</span>
     </p>
-    <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
-    <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
-    <a class="film-card__comments">5 comments</a>
+    <img src="./images/posters/${param.film_info.poster}" alt="" class="film-card__poster">
+    <p class="film-card__description">${param.film_info.description.trimEllip(139)}</p>
+    <a class="film-card__comments">${param.comments.length} comments</a>
     <div class="film-card__controls">
       <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
       <button class="film-card__controls-item film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
       <button class="film-card__controls-item film-card__controls-item--favorite" type="button">Mark as favorite</button>
     </div>
-  </article>`
-);
+  </article>`;
+};
