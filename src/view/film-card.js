3c0655a9435751fdc4c1d85afd730d874ee887f3
Andrey <danilenko.a.g@mail.ru>
2021-08-03 01:32:37 +0300

    Меняем название функции и переписывем обрезку текста

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -1,17 +1,21 @@
-export const createFilmCardTemplate = (param) => {
-  String.prototype.trimEllip = function (length) {
-    return this.length > length ? `${this.substring(0, length)}...` : this;
-  };
+export const createFilmCardTemplate = (params) => {
+  const getFirstArr = (arr) => arr[0];
 
-  const stringArr = (arr) => arr[0];
-
-  const { title, runtime, genre, poster, description } = param.film_info;
-  const rating = param.film_info.total_rating;
-  const date = param.film_info.release.date;
-  const comments = param.comments.length;
-  const { watchlist, favorite } = param.user_details;
-  const history = param.user_details.already_watched;
+  const { title, runtime, genre, poster, description } = params.film_info;
+  const rating = params.film_info.total_rating;
+  const date = params.film_info.release.date;
+  const comments = params.comments.length;
+  const { watchlist, favorite } = params.user_details;
+  const history = params.user_details.already_watched;
   const className = (variable) => variable ? 'film-card__controls-item film-card__controls-item--active' : 'film-card__controls-item';
+
+  const getSliceText = (text) => {
+    let newText = text.slice(0, 139);
+    if (text.length > newText.length) {
+      newText += '...';
+    }
+    return newText;
+  };
 
   return `<article class="film-card">
     <h3 class="film-card__title">${title}</h3>
@@ -19,10 +23,10 @@ export const createFilmCardTemplate = (param) => {
     <p class="film-card__info">
       <span class="film-card__year">${date}</span>
       <span class="film-card__duration">${runtime}</span>
-      <span class="film-card__genre">${stringArr(genre)}</span>
+      <span class="film-card__genre">${getFirstArr(genre)}</span>
     </p>
     <img src="./images/posters/${poster}" alt="" class="film-card__poster">
-    <p class="film-card__description">${description.trimEllip(139)}</p>
+    <p class="film-card__description">${getSliceText(description)}</p>
     <a class="film-card__comments">${comments} comments</a>
     <div class="film-card__controls">
       <button class="${className(watchlist)} film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
