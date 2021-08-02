d207dbaece19c69df6b4d206afcbb19f2289fdca
Andrey <danilenko.a.g@mail.ru>
2021-08-02 22:31:21 +0300

    Завершаем динамичность карточки

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -5,21 +5,29 @@ export const createFilmCardTemplate = (param) => {
 
   const stringArr = (arr) => arr[0];
 
+  const { title, runtime, genre, poster, description } = param.film_info;
+  const rating = param.film_info.total_rating;
+  const date = param.film_info.release.date;
+  const comments = param.comments.length;
+  const { watchlist, favorite } = param.user_details;
+  const history = param.user_details.already_watched;
+  const className = (variable) => variable ? 'film-card__controls-item film-card__controls-item--active' : 'film-card__controls-item';
+
   return `<article class="film-card">
-    <h3 class="film-card__title">${param.film_info.title}</h3>
-    <p class="film-card__rating">${param.film_info.total_rating}</p>
+    <h3 class="film-card__title">${title}</h3>
+    <p class="film-card__rating">${rating}</p>
     <p class="film-card__info">
-      <span class="film-card__year">${param.film_info.release.date}</span>
-      <span class="film-card__duration">${param.film_info.runtime}</span>
-      <span class="film-card__genre">${stringArr(param.film_info.genre)}</span>
+      <span class="film-card__year">${date}</span>
+      <span class="film-card__duration">${runtime}</span>
+      <span class="film-card__genre">${stringArr(genre)}</span>
     </p>
-    <img src="./images/posters/${param.film_info.poster}" alt="" class="film-card__poster">
-    <p class="film-card__description">${param.film_info.description.trimEllip(139)}</p>
-    <a class="film-card__comments">${param.comments.length} comments</a>
+    <img src="./images/posters/${poster}" alt="" class="film-card__poster">
+    <p class="film-card__description">${description.trimEllip(139)}</p>
+    <a class="film-card__comments">${comments} comments</a>
     <div class="film-card__controls">
-      <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
-      <button class="film-card__controls-item film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
-      <button class="film-card__controls-item film-card__controls-item--favorite" type="button">Mark as favorite</button>
+      <button class="${className(watchlist)} film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
+      <button class="${className(history)} film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
+      <button class="${className(favorite)} film-card__controls-item--favorite" type="button">Mark as favorite</button>
     </div>
   </article>`;
 };
