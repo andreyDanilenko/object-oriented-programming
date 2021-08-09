f61343c258614eff7a3003d2475438f7d580a196
Andrey <danilenko.a.g@mail.ru>
2021-08-09 15:21:26 +0300

    меняем фильтрацию

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,12 +1,12 @@
 const cardToFilterMap = {
-  Favorites: (tasks) => tasks.filter((task) => task.userDetails.favorite).length,
-  History: (tasks) => tasks.filter((task) => task.userDetails.history).length,
-  Watchlist: (tasks) => tasks.filter((task) => task.userDetails.watchlist).length,
+  Favorites: 'favorite',
+  History: 'history',
+  Watchlist: 'watchlist',
 };
 
 export const generateFilter = (tasks) => Object.entries(cardToFilterMap).map(
   ([filterName, countTasks]) => ({
     name: filterName,
-    count: countTasks(tasks),
+    count: tasks.filter((task) => task.userDetails[countTasks]).length,
   }),
 );
