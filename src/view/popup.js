4f0b774aad336297df3a96bd8d309ade08976d81
Andrey <danilenko.a.g@mail.ru>
2021-09-07 19:53:51 +0300

    Отображаем постеры

--- src/view/popup.js
+++ src/view/popup.js
@@ -51,7 +51,7 @@ const createPopupTemplate = (data, dataComment) => {
           </div>
           <div class="film-details__info-wrap">
             <div class="film-details__poster">
-              <img class="film-details__poster-img" src="./images/posters/${poster}" alt="">
+              <img class="film-details__poster-img" src="${poster}" alt="">
 
                 <p class="film-details__age">${ageRating}</p>
           </div>
